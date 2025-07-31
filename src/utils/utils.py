ec2a8974062cbab1d4d1c29ef617b3e300a33ee2
skier-song9 <0603paul@gmail.com>
2025-08-01 01:12:44 +0900

    feat: checkout from base1 & update notebooks

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -4,5 +4,5 @@ from zoneinfo import ZoneInfo
 def get_current_time():
     korea_timezone = ZoneInfo('Asia/Seoul')
     now_kst = datetime.now(korea_timezone)
-    formatted_time = now_kst.strftime("%m%d%H%M%S")
+    formatted_time = now_kst.strftime("%m%d%H%M")
     return formatted_time
