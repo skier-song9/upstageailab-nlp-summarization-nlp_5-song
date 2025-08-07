b40dac4596c037e0d737a640da89e4b51ad97061
skier-song9 <0603paul@gmail.com>
2025-08-07 01:25:45 +0000

    Initialize project

--- code/core/__init__.py
+++
@@ -1,4 +0,0 @@
-# core 모듈 초기화
-from .inference import InferenceEngine
-
-__all__ = ['InferenceEngine']
--- code/preprocessing/__init__.py
+++
@@ -1,8 +0,0 @@
-"""전처리 모듈"""
-
-from .text_normalizer import TextNormalizer, normalize_batch
-
-__all__ = [
-    'TextNormalizer',
-    'normalize_batch'
-]
